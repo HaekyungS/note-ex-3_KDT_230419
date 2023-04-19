4732b07f9eebe2d71ff1051587d975d4e4371a25
HaekyungSeong <haekyungs95@gmail.com>
2023-04-19 20:01:50 +0900

    기본 틀 작성

---
+++ 성해경.js
@@ -0,0 +1,49 @@
+let basicData = {
+  header:{},
+  main:{},
+  footer:{}
+}
+
+
+let fromJaonData = {
+  header:{
+    content:"header 부분 입니다.",
+    style:{
+      width:"100vw",
+      height:"100px",
+      backgroundColor:"#ccc"
+    }
+  },
+  main:{
+    content:"main 부분 입니다.",
+    style:{
+      width:"100vw",
+      height:"100px",
+      backgroundColor:"#333"
+    }
+  },
+  footer:{
+    content:"footer 부분 입니다.",
+    style:{
+      width:"100vw",
+      height:"100px",
+      backgroundColor:"#666"
+    }
+  }
+}
+
+function exampleOne(basicData, fromJaonData){
+  let value = "";
+
+  return value
+}
+
+const element="";
+element.innerHTML = exampleOne(basicData, fromJaonData);
+
+
+class ExampleTwo{
+  constructor(){
+    
+  }
+}
